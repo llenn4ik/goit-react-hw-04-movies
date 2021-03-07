5b54421a6fc90e1c9315837a804ae24240c095db
llenn4ik <llenn4ik@gmail.com>
2021-03-07 17:45:00 +0200

    Updates

---
+++ precache-manifest.37d9f9b95899ed409173effe6570f85c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769d11fed7ddddb63edfde435cc051a5",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "a6bc2d720ecbb0406a72",
+    "url": "/goit-react-hw-04-movies/static/css/6.2146a14a.chunk.css"
+  },
+  {
+    "revision": "c1ec2b6641d67aa19bae",
+    "url": "/goit-react-hw-04-movies/static/css/main.172a762a.chunk.css"
+  },
+  {
+    "revision": "a3c3a1e15be61603fff4",
+    "url": "/goit-react-hw-04-movies/static/js/0.a4aa27a6.chunk.js"
+  },
+  {
+    "revision": "a6bc2d720ecbb0406a72",
+    "url": "/goit-react-hw-04-movies/static/js/6.2f02dfec.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/goit-react-hw-04-movies/static/js/6.2f02dfec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a530e074f89d6da6233",
+    "url": "/goit-react-hw-04-movies/static/js/DetailsPage-page.9df88b1b.chunk.js"
+  },
+  {
+    "revision": "ae231be8dac4ec2b3751",
+    "url": "/goit-react-hw-04-movies/static/js/Movies-page.0073ae85.chunk.js"
+  },
+  {
+    "revision": "d6a1a3ac80ea92078d6f",
+    "url": "/goit-react-hw-04-movies/static/js/home-page.43146ba4.chunk.js"
+  },
+  {
+    "revision": "c1ec2b6641d67aa19bae",
+    "url": "/goit-react-hw-04-movies/static/js/main.c4b522ad.chunk.js"
+  },
+  {
+    "revision": "503b52b4b307cd612ca7",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.954d25ba.js"
+  }
+]);
